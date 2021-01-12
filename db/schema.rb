c118e90dfc7e280b13b826a8fa7600c6b7ab2d2b
daniel <dany.debug@gmail.com>
2021-01-12 17:53:40 +0530

    added some sessions creation code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_153313) do
+ActiveRecord::Schema.define(version: 2021_01_12_104419) do
 
   create_table "members", force: :cascade do |t|
     t.string "first_name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_01_07_153313) do
     t.string "email"
     t.string "phone"
     t.string "skill"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
